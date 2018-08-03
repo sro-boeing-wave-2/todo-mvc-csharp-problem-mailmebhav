264f37404e436facf9a6c6b92b4a4f1265f927ef
Bhavin Patel <mailmebhav@gmail.com>
2018-08-03 12:35:51 +0530

    Initial commit

---
+++ NotesApi/NotesApi/Data/NotesApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotesApi.Models
+{
+    public class NotesApiContext : DbContext
+    {
+        public NotesApiContext (DbContextOptions<NotesApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Notes> Notes { get; set; }
+        //public DbSet<Checklists> Checklists { get; set; }
+        //public DbSet<Labels> Labels { get; set; }
+    }
+}
---
+++ NotesApi/NotesApi/Models/Checklists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesApi.Models
+{
+    public class Checklists
+    {
+        public int ID { get; set; }
+        public string Item { get; set; }
+    }
+}
